6c30b653bd1d37e2875c58e92f109966cc1fc71f
baabak1990 <baabakaghaei@gmail.com>
2022-09-17 16:40:34 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+// See https://aka.ms/new-console-template for more information
+
+//Late binding Training
+// Execute The assemblly you want First
+Assembly GetassymblyType=Assembly.GetExecutingAssembly();
+//Secondly get type of that object and try name it after class or other object you want
+ Type GetTestReflactorType=GetassymblyType.GetType("Reflactor.TestReflactor");
+//After we got our type it is time to create an instance of it
+//Just takecare about relations and namings
+object GetInstanceOfTestClass=Activator.CreateInstance(GetTestReflactorType);
---
+++ TestReflactor.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflactor
+{
+    
+    //public class TestReflactor
+    //{
+    //    public string Name { get; set; }=string.Empty;
+    //    public string FullName { get; set; } = string.Empty;
+
+    //    public string SayFullName(string name,string lastname) => name+" " + lastname;
+    //}
+}
